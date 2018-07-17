beb70e4c63715402bf19e04f27ce77e1f4325541
Jack Wood <jackw@automationsquared.com>
2018-07-17 10:44:24 +0100

    v2

--- ejdev/pages/received.php
+++
@@ -1,9 +0,0 @@
-<html>
- <body>
-
- Form data:
- <?php echo $_POST["input-invoice"]; ?><br>
- <?php echo $_POST["input-quote"]; ?>
-
- </body>
- </html> 
--- ejdev/pages/www.quotecheck.tech/qc_global_preloader.js
+++
@@ -1,6 +0,0 @@
-$(".preloader").hide();
-
-$(".file-select-submit").click(function(){  /* to go back from the hour section to the day section */
-    $(".preloader").show();
-    $(".file-select-submit").hide();
-  });
